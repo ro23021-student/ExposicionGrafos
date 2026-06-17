6be21ceca1f5d58023e1fe242a99adefd0b2d5f2
my22002 <my22002@ues.edu.sv>
2026-06-17 15:47:51 -0600

    implementacion de agragar via

--- src/versionmanual.rs
+++ src/versionmanual.rs
@@ -4,3 +4,17 @@ use colored::*;
 pub fn ejecutar(){
     
 }
+
+
+// Conecta dos estaciones mediante una vía.
+    // Como el grafo es NO DIRIGIDO, la conexión se registra en ambos sentidos
+    // (si A llega a B, entonces B también llega a A).
+
+    fn agregar_via(&mut self, a: usize, b: usize) {
+       // A puede llegar a B.
+       self.adyacencia[a].push(b);
+       // B puede llegar a A.
+       self.adyacencia[b].push(a);
+   }
+
+
